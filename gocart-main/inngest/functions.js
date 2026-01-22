79d470a44695d8a0921212813f81f4be59035412
Rim <rimtarhini@gmail.com>
2026-01-22 13:50:29 +0200

    first commit

---
+++ gocart-main/inngest/functions.js
@@ -0,0 +1,50 @@
+import {inngest} from './client'
+import prisma from '@/lib/prisma'
+
+
+// Create user in database when created in Clerk
+export const syncUserCreation = inngest.createFunction(
+    {id: 'sync-user-create'},
+    {event: 'clerk/user.created'},
+    async ({event}) => {
+        const {data} = event 
+        await prisma.user.create({
+            data: {
+                id: data.id,
+                email: data.email_addresses[0].email_address,
+                name: `${data.first_name} ${data.last_name}` ,
+                image: data.image_url,
+            }
+        })
+    }
+)
+
+// Update user in database when updated in Clerk
+export const syncUserUpdation = inngest.createFunction(
+    {id: 'sync-user-update'},
+    {event: 'clerk/user.updated'},
+    async ({event}) => {
+        const {data} = event 
+        await prisma.user.update({
+            where: {id: data.id},
+            data: {
+                email: data.email_addresses[0].email_address,
+                name: `${data.first_name} ${data.last_name}` ,
+                image: data.image_url,
+            }
+        })
+    }
+)
+
+
+// Delete user from database when deleted from Clerk
+export const syncUserDeletion = inngest.createFunction(
+    {id: 'sync-user-delete'},
+    {event: 'clerk/user.deleted'},
+    async ({event}) => {
+        const {data} = event 
+        await prisma.user.delete({
+            where: {id: data.id},
+        })
+    }
+)
