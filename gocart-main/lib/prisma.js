e84fa45c6e070e08eda7ec6546e380c88bd57906
Rim <rimtarhini@gmail.com>
2026-01-22 16:07:33 +0200

    fixed prisma.js

--- gocart-main/lib/prisma.js
+++ gocart-main/lib/prisma.js
@@ -1,23 +1,21 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaNeon } from '@prisma/adapter-neon';
-import { neonConfig } from '@neondatabase/serverless';
+import { Pool, neonConfig } from '@neondatabase/serverless';
 
 import ws from 'ws';
 neonConfig.webSocketConstructor = ws;
 
 // To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
-neonConfig.poolQueryViaFetch = true
+neonConfig.poolQueryViaFetch = true;
 
-// Type definitions
-// declare global {
-//   var prisma: PrismaClient | undefined
-// }
+const connectionString = process.env.DATABASE_URL;
 
-const connectionString = `${process.env.DATABASE_URL}`;
+// Create Pool instance and pass it to PrismaNeon
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
 
-const adapter = new PrismaNeon({ connectionString });
 const prisma = global.prisma || new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 export default prisma;
